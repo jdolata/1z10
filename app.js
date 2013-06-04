abb281c6f4c72f15524293ecfa69aeb60f8021f7
Jedrzej Dolata <cerin.n1@gmail.com>
2013-06-04 23:42:55 +0200

    strona glowna,zasady,modulu v0.1

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+/*jshint node: true */
+'use strict';
+var express = require('express');
 var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var path = require('path');
+var app = express();
+//var serverGameLogic = require('./Public/Scripts/script');
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.favicon());
+   // app.use(express.logger());
+
+    app.use(express.static(path.join(__dirname, 'Public')));
+});
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/Public/index.html');
+});
+
+app.get('/zasady', function (req, res) {
+    res.sendfile(__dirname + '/Public/rules.html');
+});
+
+app.get('/congratulations', function (req, res) {
+    res.sendfile(__dirname + '/Public/congratulations.html');
+});
+
+var server = http.createServer(app).listen(app.get('port'));
+
+var io = require('socket.io');
+var socket = io.listen(server);
+socket.set('log level', 1);
